a2a1a6619e809f90b388c988bfe92e543121642d
AlfonsoRomero1992 <128775642+AlfonsoRomero1992@users.noreply.github.com>
2023-04-27 18:10:24 -0500

    cambios 2do parcial

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,17 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
+  getproducts(limit: number, offset: number): string {
+    const until: number = Number(limit) + Number(offset);
+    return 'Productos desde el id ' + ' hasta ' + until;
+  }
+  getHelloAndName(name: string): string {
+    return this.getHello() + ' Tu nombre es ' + name;
+  }
   getHello(): string {
-    return 'Hello World!';
+    return 'Hello Word!';
+  }
+  getname(): string {
+    return 'Mi nombre es Alfonso';
   }
 }
